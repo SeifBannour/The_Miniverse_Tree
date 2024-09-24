1dfbf32acde74f0752b71f18344078b8789ad9e3
SeifBannour <123808873+SeifBannour@users.noreply.github.com>
2024-09-24 18:48:05 +0100

    v0.01

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Miniverse Tree",
+	id: "tminit",
+	author: "SomeRandomGuy",
+	pointsName: "?",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.01",
+	name: "still nothing",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
